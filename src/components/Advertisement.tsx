2605239e06a23788c8931c12f3b30bd0d799ff8b
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-02-01 19:06:03 +0900

    refactor: electron 로직 조정

--- src/components/Advertisement.tsx
+++
@@ -1,7 +0,0 @@
-export default function Advertisement() {
-  return (
-    <div className="h-[100px] w-[600px]">
-      <iframe src="https://www.andongmin.com/ad/kersor.png" className="h-full w-full" />
-    </div>
-  );
-}
