530364b03253284e524e9933d6db7267801f68de
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-04-09 10:46:23 +0900

    refactor: electron 로직 조정

--- src/electron/preload.cjs
+++ src/electron/preload.cjs
@@ -1,9 +1,31 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld('electron', {
-  send: (channel, data) => ipcRenderer.send(channel, data),
-  on: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-  removeListener: (channel, func) => ipcRenderer.removeListener(channel, func),
-  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
-  get: (key) => ipcRenderer.invoke('get-store-value', key),
+  send: (channel, data) => {
+    // 허용된 채널 목록
+    let validChannels = ["minimize", "hidden", "update-settings", "request-displays"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  on: (channel, func) => {
+    let validChannels = ["displays-updated", "update-settings", "mouse-move", "key-press", "init"];
+    if (validChannels.includes(channel)) {
+      // Strip event as it includes `sender` and is a security risk
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  },
+  removeListener: (channel, func) => {
+    let validChannels = ["displays-updated", "update-settings", "mouse-move", "key-press", "init"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.removeListener(channel, func);
+    }
+  },
+  removeAllListeners: (channel) => {
+    let validChannels = ["displays-updated", "update-settings", "mouse-move", "key-press", "init"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.removeAllListeners(channel);
+    }
+  },
+  get: () => ipcRenderer.invoke('get-settings'),
 });
