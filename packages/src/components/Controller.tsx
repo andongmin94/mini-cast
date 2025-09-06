925d6cea396e96a348861771efa445869835a403
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-09-06 12:44:56 +0900

    build: 패키지 설정 정리 packages package.json

--- packages/src/components/Controller.tsx
+++ packages/src/components/Controller.tsx
@@ -515,6 +515,12 @@ export default function Controller() {
           className="h-full w-full border-0"
           sandbox="allow-scripts allow-same-origin"
         />
+        <a
+          href="https://mini-cast.andongmin.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute top-0 left-0 h-full w-full"
+        />
       </div>
       <div className="pb-[121px]"> {/* 기존 컨텐츠 래퍼 */}</div>
     </>
