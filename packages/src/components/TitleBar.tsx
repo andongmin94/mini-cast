1f9cf95d57485921f8cda77974cf51f8665b2f43
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-09-24 20:45:29 +0900

    docs: 문서 정리 docs

--- packages/src/components/TitleBar.tsx
+++ packages/src/components/TitleBar.tsx
@@ -5,7 +5,7 @@ import { Minus, X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
-import MiniCastLogo from "/mini-cast.svg";
+import MiniCastLogo from "/logo.png";
 
 const buttonVariants = cva(
   "inline-flex items-center hover:cursor-pointer justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
