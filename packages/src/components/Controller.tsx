87cde5d036cddb36c094dc824ffae5cd751e072b
안동민 <andongmin94@gmail.com>
2026-01-29 15:16:42 +0900

    v0.1.9 다이얼로그가 tabs보다 z축 위에가게

--- packages/src/components/Controller.tsx
+++ packages/src/components/Controller.tsx
@@ -14,7 +14,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+} from "@/components/ui-custom/alert-dialog";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import {
